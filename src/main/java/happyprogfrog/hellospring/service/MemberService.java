f78c42c7ec0d37ffffa32ffe3148209d33b83f16
happyprogfrog <happyprogfrog@gmail.com>
2024-04-16 09:56:44 +0900

    컴포넌트 스캔과 자동 의존 관계 설정

---
+++ src/main/java/happyprogfrog/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package happyprogfrog.hellospring.service;
+
+import happyprogfrog.hellospring.domain.Member;
+import happyprogfrog.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        // 내가 직접 new 하지 않고, 외부에서 넣어줌
+        // 이런걸 Dependency Injection 즉, 의존성 주입이라고 한다!
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 단일 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
